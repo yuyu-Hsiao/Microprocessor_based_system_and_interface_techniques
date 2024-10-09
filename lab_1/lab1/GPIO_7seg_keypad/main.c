cf2e5c962a9ff723433d00a703ee87ee743141bc
YuYu <horace70112@gmail.com>
2024-10-10 04:38:35 +0800

    111

---
+++ lab_1/lab1/GPIO_7seg_keypad/main.c
@@ -0,0 +1,41 @@
+//
+// GPIO_7seg_keypad : 3x3 keypad inpt and display on 7-segment LEDs
+//
+#include <stdio.h>
+#include "NUC100Series.h"
+#include "MCU_init.h"
+#include "SYS_init.h"
+#include "Seven_Segment.h"
+#include "Scankey.h"
+
+uint8_t id[4] = {3, 1, 0, 5};
+
+// display an integer on four 7-segment LEDs
+void Display_7seg(uint16_t value)
+{
+	uint8_t i;
+	if(value!=0){
+		id[0] = value;
+	}else{
+		id[0] = 3;
+	}
+	
+  for(i=0;i<4;i++){
+		CloseSevenSegment();
+		ShowSevenSegment(i,id[i]);
+		CLK_SysTickDelay(5000);		
+	}
+}
+
+int main(void)
+{
+		uint16_t j;
+    SYS_Init();
+    OpenSevenSegment();
+	  OpenKeyPad();
+
+ 	  while(1) {
+		j=ScanKey();
+    Display_7seg(j);
+	  }
+}
